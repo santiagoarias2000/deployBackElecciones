26684a6df40da419578ee4f443dd4843d71b9867
santiagoarias2000 <santiago.ariasm@gmail.com>
2023-04-21 15:40:20 -0500

    Update to backend finally

--- src/repository/concejo/sql_details_especific_concejo.js
+++ src/repository/concejo/sql_details_especific_concejo.js
@@ -7,12 +7,13 @@ exports.SQL_DETAILS_ESPECIFICS_CONCEJO = {
     FROM votes_concejo_19 \
     INNER JOIN municipality on municipality.id_municipality = votes_concejo_19.id_municipality \
     WHERE municipality.id_department = $1 \
-    GROUP BY municipality.id_municipality,municipality.name_municipality,municipality.id_department",
+    GROUP BY municipality.id_municipality,municipality.name_municipality,municipality.id_department\
+    ORDER BY municipality.name_municipality ASC",
     CONCEJO_DEPARTAMENTO_MUNICICPIO: "SELECT vcon.candidate_name,vcon.description_politicparty, SUM(vcon.number_votes) AS VOTOS,\
     jsonb_object(array['id_municipality',mu.id_municipality,'name_municipality',mu.name_municipality]::TEXT[]) AS municipality \
     FROM votes_concejo_19 vcon \
     INNER JOIN municipality mu ON mu.id_municipality = vcon.id_municipality \
     WHERE vcon.id_municipality= $1 \
     GROUP by vcon.description_politicparty, vcon.candidate_name, mu.id_municipality, mu.name_municipality \
-    ORDER BY vcon.description_politicparty,votos desc",
+    ORDER BY vcon.description_politicparty,votos DESC",
 };
--- src/repository/jal/sql_details_especific_jal.js
+++ src/repository/jal/sql_details_especific_jal.js
@@ -7,12 +7,13 @@ exports.SQL_DETAILS_ESPECIFICS_JAL = {
     FROM votes_jal_19 \
     INNER JOIN municipality on municipality.id_municipality = votes_jal_19.id_municipality \
     WHERE municipality.id_department = $1 \
-    GROUP BY municipality.id_municipality,municipality.name_municipality,municipality.id_department",
+    GROUP BY municipality.id_municipality,municipality.name_municipality,municipality.id_department\
+    ORDER BY municipality.name_municipality ASC",
     JAL_DEPARTAMENTO_MUNICICPIO: "SELECT vjal.candidate_name,vjal.description_politicparty, SUM(vjal.number_votes) AS VOTOS, \
     jsonb_object(array['id_municipality',mu.id_municipality,'name_municipality',mu.name_municipality]::TEXT[]) AS municipality \
     FROM votes_jal_19 vjal \
     INNER JOIN municipality mu ON mu.id_municipality = vjal.id_municipality \
     WHERE vjal.id_municipality= $1 \
     GROUP by vjal.description_politicparty, vjal.candidate_name, mu.id_municipality, mu.name_municipality \
-    ORDER BY vjal.description_politicparty,votos desc"
+    ORDER BY vjal.description_politicparty,votos DESC"
 };
